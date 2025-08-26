ccdef36d2e9abaa1b4050f8709fe3d346b35e151
AmishaNaik7 <137600541+AmishaNaik7@users.noreply.github.com>
2025-08-26 12:54:15 +0100

    Add files via upload

---
+++ DevaluacionCilindros.cpp
@@ -0,0 +1,26 @@
+#include "DevaluacionCilindros.h"
+
+std::map<int, double> DevaluacionCilindros::tabla = {
+    {3, 0.02},
+    {4, 0.04},
+    {8, 0.06},
+    {12, 0.08}
+};
+
+DevaluacionCilindros::DevaluacionCilindros()
+{
+}
+
+DevaluacionCilindros::~DevaluacionCilindros()
+{
+}
+
+bool DevaluacionCilindros::buscarCilindros(const int cilindrada)
+{
+    return (tabla.find(cilindrada) != tabla.end());
+}
+
+double DevaluacionCilindros::obtenerDevaluacion(const int cilindrada)
+{
+    return tabla.at(cilindrada);
+}
